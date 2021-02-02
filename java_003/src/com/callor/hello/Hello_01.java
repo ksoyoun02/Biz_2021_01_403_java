789802b5a1e9897911889cf88287c004243fa1c5
unknown <ksoyoun95@naver.com>
2021-02-02 14:12:11 +0900

    명령어

---
+++ java_003/src/com/callor/hello/Hello_01.java
@@ -0,0 +1,28 @@
+package com.callor.hello;
+
+public class Hello_01 {
+
+	public static void main(String args[]) {
+
+		System.out.println("여기는 Hello_01 클래스");
+		System.out.println("여기는 Hello_O1 클래스");
+		System.out.println();
+		
+		System.out.print("30 + 40 = " );
+		System.out.println(30 +40);
+		
+		System.out.print("Republic ");
+		System.out.print("of ");
+		System.out.println("Korea");
+
+		
+		// sysout + ctrl + space -> System.out.println(); 가 나옴
+		// 0 O 차이 체크! ( l i  -> D2coding 때문에 구분가능!!)
+		// ln은 Enter효과 (print+ln)  => New line!!
+		// ptint는 ln이 없어서 그 아랫줄과 줄바꿈없이 한줄로 이어짐
+		// "문자열" + 숫자 로 하면 식이 복잡해지니까!
+		
+
+	}
+
+}
