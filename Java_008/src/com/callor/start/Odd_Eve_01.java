e3e64eaf218898dd3f5cb95a3bdef88eb5122ff6
ksoyoun02 <ksoyoun95@naver.com>
2021-02-08 13:25:51 +0900

    EvenOdd

--- Java_008/src/com/callor/start/Odd_Eve_01.java
+++ Java_008/src/com/callor/start/Odd_Eve_01.java
@@ -7,12 +7,13 @@ public class Odd_Eve_01 {
 		int intsum = 0;
 		// 0~99
 		for (int i = 0; i < 100; i++) {
-			boolean bYes = (i+1) % 2 == 0;
-
+			
+			int num = i + 1;
+			boolean bYes = num % 2 == 0;
+			
 			// 만약 bYes true 이면
 			if (bYes) {
 
-				int num = i + 1;
 				intsum += num;
 
 			}
@@ -21,16 +22,37 @@ public class Odd_Eve_01 {
 
 		intsum = 0;
 		for (int i = 0; i < 100; i++) {
-			boolean bYes = i % 2 == 1;
-
+			
+			int num = i + 1;
+			boolean bYes = num % 2 == 1;
+			
 			if (bYes) {
 
-				int num = i;
 				intsum += num;
 
 			}
 		}
 		System.out.println("1 ~ 100까지 홀수의 합 : " + intsum);
+		
+		//위에서 선언하고 사용했던 변수를 "재 사용"하기
+		// 반드시 초기화(clear)해야한다
+		intsum = 0;
+		for(int i = 0 ; i < 100 ; i++) {
+			int num = i + 1;
+			boolean bYes = num % 2 == 0;
+			
+			
+			// if( bYes == false ) {
+			
+			// bYes가 true가 아니면	
+			if( !bYes ) {
+				
+				intsum += num;
+				
+			}
+		}
+		System.out.println("홀수의 합 : " + intsum);
+		
 
 		// 값을 변수에 계속 더하라
 
