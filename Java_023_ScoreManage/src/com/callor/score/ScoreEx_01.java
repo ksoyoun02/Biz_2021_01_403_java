21aa36be787aa98044f42b3a3082f5884bc613c1
unknown <ksoyoun95@naver.com>
2021-03-05 23:50:04 +0900

    과제

--- Java_023_ScoreManage/src/com/callor/score/ScoreEx_01.java
+++ Java_023_ScoreManage/src/com/callor/score/ScoreEx_01.java
@@ -1,15 +1,31 @@
 package com.callor.score;
 
 import com.callor.score.service.MenuService;
+import com.callor.score.service.ScoreService;
 import com.callor.score.service.impl.MenuServiceImplV1;
+import com.callor.score.service.impl.ScoreServiceImplV1;
 
 public class ScoreEx_01 {
 	
 	public static void main(String[] args) {
 		
 		MenuService mService = new MenuServiceImplV1();
+		ScoreService sService = new ScoreServiceImplV1();
 		
-		mService.selectMenu();
+		while(true) {
+			Integer menuItem = mService.selectMenu();
+			if(menuItem == null) {
+				break;
+			}else if(menuItem == 1) {
+				sService.makeScore();
+			}else if(menuItem == 2) {
+				sService.saveScoreToFile();
+			}else if(menuItem == 3) {
+				sService.loadScoreFrom();
+			}
+			
+		}
+		
 	}
 
 }
--- Java_023_ScoreManage/src/com/callor/score/files/FileReader_01.java
+++ Java_023_ScoreManage/src/com/callor/score/files/FileReader_01.java
@@ -1,82 +1,22 @@
-package com.callor.score.service.impl;
+package com.callor.score.files;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import com.callor.score.model.ScoreVO;
-import com.callor.score.service.ScoreService;
 import com.callor.score.values.Values;
 
-public class ScoreServiceImplV1 implements ScoreService {
+public class FileReader_01 {
 
-	
-	// save, load method가 공용으로 사용할 객체
-	private List<Integer> intList;
-	private String scoreFile;
-
-	public ScoreServiceImplV1() {
-
-		intList = new ArrayList<Integer>();
-		scoreFile = "src/com/callor/score/files/nums_rnd.txt";
-
-	}
-
-	@Override
-	public void makeScore() {
-		// Random 수를 생성하여 List에 담을 method
-		Random rnd = new Random();
-		for (int i = 0; i < 100; i++) {
-			Integer num = rnd.nextInt(100) + 1;
-			intList.add(num);
-		}
-
-	}
-
-	@Override
-	public void saveScoreToFile() {
-		// List에 담긴 숫자들을 파일에 저장하는 mehtod
-		FileWriter fileWriter = null;
-		PrintWriter printer = null;
-
-		try {
-			fileWriter = new FileWriter(scoreFile);
-			printer = new PrintWriter(fileWriter);
-
-			int nSize = intList.size();
-			for (int i = 0; i < nSize; i++) {
-				Integer n = intList.get(i);
-
-				printer.print(n + ":");
-
-				if ((i + 1) % 5 == 0) {
-					printer.println();
-				}
-			}
-
-			printer.close();
-			fileWriter.close();
-		}
-
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-
-		}
-
-	}
-
-	@Override
-	public void loadScoreFrom() {
+	public static void main(String[] args) {
 
 		List<ScoreVO> scoreList = new ArrayList<ScoreVO>();
 		List<String> strLines = new ArrayList<String>();
+		String scoreFile = "src/com/callor/score/files/nums_rnd.txt";
 
 		FileReader fileReader = null;
 		BufferedReader buffer = null;
@@ -101,8 +41,8 @@ public class ScoreServiceImplV1 implements ScoreService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		for (String str : strLines) {
+		
+		for(String str : strLines) {
 			String[] scores = str.split(":");
 			ScoreVO scoreVO = new ScoreVO();
 			scoreVO.setKor(Integer.valueOf(scores[0]));
@@ -112,25 +52,25 @@ public class ScoreServiceImplV1 implements ScoreService {
 			scoreVO.setMusic(Integer.valueOf(scores[4]));
 			scoreList.add(scoreVO);
 		}
-
-		for (ScoreVO vo : scoreList) {
+		
+		for(ScoreVO vo : scoreList) {
 			int sum = vo.getKor();
 			sum += vo.getEng();
 			sum += vo.getMath();
 			sum += vo.getMusic();
 			sum += vo.getHistory();
-
-			float avg = (float) sum / 5;
-
+			
+			float avg = (float)sum / 5;
+			
 			vo.setTotal(sum);
 			vo.setAvg(avg);
 		}
 		System.out.println(Values.dLine);
 		System.out.println("순번\t국어\t영어\t수학\t음악\t국사\t총점\t평균");
 		System.out.println(Values.sLine);
-
+		
 		int nSize = scoreList.size();
-		for (int i = 0; i < nSize; i++) {
+		for(int i = 0 ; i < nSize; i++) {
 			ScoreVO vo = scoreList.get(i);
 			int num = i + 1;
 			System.out.print(num + "\t");
@@ -141,7 +81,7 @@ public class ScoreServiceImplV1 implements ScoreService {
 			System.out.print(vo.getHistory() + "\t");
 			System.out.print(vo.getTotal() + "\t");
 			System.out.print(vo.getAvg() + "\n");
-
+			
 		}
 		System.out.println(Values.dLine);
 
